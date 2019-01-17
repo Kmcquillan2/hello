4eb79b687b19418a657561d878ef800d4287d90b
Kmcquillan2 <kmcquillan1999@gmail.com>
2019-01-17 11:28:06 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Kaleb McQuillan, Michael Brandt
+ * Date: 2018/1/16
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kaleb McQuillan, Michael Brandt\n");
+
+  return 0;
+}
